8cf77bf3f21435ac95dcb01e37ff7de0c0ff774f
Qian Long <hungryha@gmail.com>
2013-01-08 23:28:46 -0500

    added defensive minebuilding to nukeplayer

--- nukeplayer/RobotPlayer.java
+++ nukeplayer/RobotPlayer.java
@@ -1,23 +1,59 @@
 package nukeplayer;
 
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
-import battlecode.common.Upgrade;
+import battlecode.common.*;
 
 public class RobotPlayer {
-	public static void run(RobotController rc) {
+	private static RobotController rc;
+	private static Team myTeam;
+	private static Team otherTeam;
+
+	public static void run(RobotController myRC) {
+		rc = myRC;
+		myTeam = rc.getTeam();
+		otherTeam = myTeam.opponent();
+		Direction enemyBaseDir = rc.getLocation().directionTo(
+				rc.senseEnemyHQLocation());
 		while (true) {
 			try {
 				if (rc.getType() == RobotType.HQ) {
 					System.out.println("hq robot type");
 					if (rc.isActive()) {
-						rc.researchUpgrade(Upgrade.NUKE);
+						if (Clock.getRoundNum() < 200) {
+							// spawn robots
+							if (rc.canMove(enemyBaseDir)) {
+								rc.spawn(enemyBaseDir);
+							}
+						} else {
+							rc.researchUpgrade(Upgrade.NUKE);
+						}
 					}
-				}
-				else if (rc.getType() == RobotType.SOLDIER) {
-					System.out.println("soldier");
-				}
-				else {
+				} else if (rc.getType() == RobotType.SOLDIER) {
+					// soldier lays mines around hq
+					if (rc.isActive()) {
+						MapLocation curLoc = rc.getLocation();
+						Team mineTeam = rc.senseMine(curLoc);
+						MapLocation mineLoc = senseAdjacentMine();
+						if (mineTeam == null) {
+							rc.layMine();
+						} else if (mineLoc != null && !rc.senseMine(mineLoc).equals(myTeam)) {
+							rc.defuseMine(mineLoc);
+						}
+						else {
+							// Choose a random direction, and move that way if
+							// possible
+							Direction dir = Direction.values()[(int) (Math
+									.random() * 8)];
+							if (rc.canMove(dir)) {
+								rc.move(dir);
+								rc.setIndicatorString(
+										0,
+										"Last direction moved: "
+												+ dir.toString());
+							}
+						}
+					}
+
+				} else {
 					System.out.println("not hq or soldier");
 				}
 
@@ -26,6 +62,36 @@ public class RobotPlayer {
 			}
 			// End turn
 			rc.yield();
+		}
+	}
+
+	private static MapLocation senseAdjacentMine() {
+		Direction dir = Direction.EAST;
+		int[] directionOffsets = { 0, 1, -1, 2, -2 };
+		MapLocation lookingAtCurrently = rc.getLocation();
+		for (int d : directionOffsets) {
+			lookingAtCurrently = rc.getLocation().add(Direction.values()[(dir.ordinal() + d + 8) % 8]);
+			if (rc.senseMine(lookingAtCurrently) != null) {
+				return lookingAtCurrently;
+			}
+		}
+		return null;
+	}
+	
+	private static void goToLocation(MapLocation whereToGo)
+			throws GameActionException {
+		int dist = rc.getLocation().distanceSquaredTo(whereToGo);
+		if (dist > 0 && rc.isActive()) {
+			Direction dir = rc.getLocation().directionTo(whereToGo);
+			int[] directionOffsets = { 0, 1, -1, 2, -2 };
+			Direction lookingAtCurrently = dir;
+			lookAround: for (int d : directionOffsets) {
+				lookingAtCurrently = Direction.values()[(dir.ordinal() + d + 8) % 8];
+				if (rc.canMove(lookingAtCurrently)) {
+					break lookAround;
+				}
+			}
+			rc.move(lookingAtCurrently);
 		}
 	}
 }
