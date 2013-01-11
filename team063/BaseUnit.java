bb0e5dd9da176ccf955492655776e9e272e3552e
David <dmbocek@mit.edu>
2013-01-10 22:18:45 -0500

    moving stuff around

--- team063/BaseUnit.java
+++ team063/BaseUnit.java
@@ -1,5 +1,8 @@
 package team063;
 
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public abstract class BaseUnit {
@@ -20,4 +23,33 @@ public abstract class BaseUnit {
 	}
 	
 	abstract public void run();
+	
+	
+	protected void goToLocationBrute(MapLocation whereToGo) throws GameActionException {
+		int dist = rc.getLocation().distanceSquaredTo(whereToGo);
+		if (dist>0&&rc.isActive()){
+			Direction dir = rc.getLocation().directionTo(whereToGo);
+			int[] directionOffsets = {0,1,-1,2,-2};
+			Direction lookingAtCurrently = dir;
+			lookAround: for (int d:directionOffsets){
+				lookingAtCurrently = Direction.values()[(dir.ordinal()+d+8)%8];
+				if(rc.canMove(lookingAtCurrently) && ((rc.senseMine(lookingAtCurrently) == null) || (rc.senseMine(lookingAtCurrently) == myTeam))){
+					rc.move(lookingAtCurrently);
+					break lookAround;
+				} else if (rc.canMove(lookingAtCurrently)){
+					rc.defuseMine(lookingAtCurrently);
+					break lookAround;
+				}
+			}
+		}
+	}
+	
+	protected void followWaypointPath(MapLocation[] waypointArray, int startIndex) throws GameActionException {
+		MapLocation currentLoc = rc.getLocation();
+		int waypointCounter = startIndex;
+		if (!currentLoc.equals(waypointArray[waypointCounter])){
+			goToLocation(waypointArray[waypointCounter]);
+		}
+		waypointCounter+=1;
+	}
 }
