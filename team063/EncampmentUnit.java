1133035c4be3d2683e8970897927324c902fb69f
David <dmbocek@mit.edu>
2013-01-14 14:17:12 -0500

    artillery basic

--- team063/EncampmentUnit.java
+++ team063/EncampmentUnit.java
@@ -1,20 +1,35 @@
 package team063;
 
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.Robot;
+import battlecode.common.GameConstants;
 
 public class EncampmentUnit extends BaseUnit {
 	public EncampmentUnit(RobotController rc) {
 		super(rc);
 	}
 
+	MapLocation enemyLoc;
 	@Override
-	public void run() {
+	public void run() throws GameActionException {
 		switch(rc.getType()) {
 		case MEDBAY:
 			break;
 		case SHIELDS:
 			break;
 		case ARTILLERY:
+			Robot[] nearbyEnemies=rc.senseNearbyGameObjects(Robot.class, 49, otherTeam);
+			if (nearbyEnemies.length >= 1){
+				for (int i=0; i<nearbyEnemies.length; i++){
+					enemyLoc=rc.senseRobotInfo(nearbyEnemies[i]).location;
+					if (rc.senseNearbyGameObjects(Robot.class, enemyLoc, 2, myTeam).length <1){
+						rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[i]).location);
+						rc.yield();
+					}
+				}
+			}
 			break;
 		case GENERATOR:
 			break;
