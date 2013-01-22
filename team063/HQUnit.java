cc06bedb4e232f2f2061d7da04af487b533c3247
DavidBocek <dmbocek@mit.edu>
2013-01-22 14:38:11 -0500

    fix on comparators to deal with null values

--- team063/HQUnit.java
+++ team063/HQUnit.java
@@ -530,6 +530,7 @@ public class HQUnit extends BaseUnit {
 		//zone1 is the zone near our base, probably only one robot per squad needed
 		@Override
 		public int compare(Object Enc0, Object Enc1){
+			if (Enc0==null){ return -1;} if (Enc1==null){ return 1;}
 			return (((MapLocation) Enc0).distanceSquaredTo(myBaseLoc)-((MapLocation) Enc1).distanceSquaredTo(myBaseLoc));
 		}
 	}
@@ -538,6 +539,7 @@ public class HQUnit extends BaseUnit {
 		//zone2 is the zone near the center of the map, may need medium sized squads and quick action to take these
 		@Override
 		public int compare(Object Enc0, Object Enc1){
+			if (Enc0==null){ return -1;} if (Enc1==null){ return 1;}
 			return ((((MapLocation) Enc0).distanceSquaredTo(myBaseLoc)+((MapLocation) Enc0).distanceSquaredTo(enemyBaseLoc))-(((MapLocation) Enc1).distanceSquaredTo(myBaseLoc)+((MapLocation) Enc1).distanceSquaredTo(enemyBaseLoc)));
 		}
 	}
@@ -546,6 +548,7 @@ public class HQUnit extends BaseUnit {
 		//zone3 is the zone near the enemy base we would want to put artillery, this should be taken when we are already pushing into the enemy lines, but probably not done immediately
 		@Override
 		public int compare(Object Enc0,Object Enc1){
+			if (Enc0==null){ return -1;} if (Enc1==null){ return 1;}
 			return ((((MapLocation) Enc0).distanceSquaredTo(enemyBaseLoc)*2+((MapLocation) Enc0).distanceSquaredTo(myBaseLoc))-(((MapLocation) Enc1).distanceSquaredTo(enemyBaseLoc)*2+((MapLocation) Enc1).distanceSquaredTo(myBaseLoc)));
 		}
 	}
@@ -554,6 +557,7 @@ public class HQUnit extends BaseUnit {
 		//zone4 is the "zone" of far away encampments that aren't near the enemy base
 		@Override
 		public int compare(Object Enc0,Object Enc1){
+			if (Enc0==null){ return -1;} if (Enc1==null){ return 1;}
 			return (((MapLocation) Enc0).distanceSquaredTo(myBaseLoc)-((MapLocation) Enc1).distanceSquaredTo(myBaseLoc));
 		}
 	}
